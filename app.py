d4f979cd8435bc2d8bbd8be8f0f8605c0191d25d
andrew <77263948+angeal1707@users.noreply.github.com>
2026-05-21 09:25:38 +0800

    ganti port

--- app.py
+++ app.py
@@ -116,5 +116,5 @@ def generate_chart(df):
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
